1e8379598e39f0640370eb13e4252d1ab54cec6a
Lrd134 <larryc3200@gmail.com>
2021-08-28 12:14:38 -0400

    commit tree 5d7069037fb09491cbf4abbb5864f0f8d139b738

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, word){
+  for (let i = 0; i < array.length; i++ ) {
+    array[i] = `Thank you, ${array[i]}, for the wonderful ${word} gift!`;
+  }
+  return array;
+}
+
+function countDown(num){
+  let counter = 0;
+  while(num >= counter){
+    console.log(num);
+    num--;
+  }
+}
